f911dddae2346ca3645a3e56ed53ec7ea44a1452
bad64 <216494@supinfo.com>
2018-09-13 19:12:45 +0200

    Added additional path check for help on Linux

--- src/misc.cpp
+++ src/misc.cpp
@@ -5,17 +5,36 @@
 QString getURL(QString language)
 {
     //This is essentially a safety measure to avoid a crash in the event that the program runs in an untranslated language
-
     struct stat buffer;
-    QString path = QString("help/" + language + "/index.html");
 
-    if (stat (path.toStdString().c_str(), &buffer) == 0)
-        return path;
-    else
-    {
-        std::cout << "Localized help for language " << language.toStdString() << " not found, defaulting to en" << std::endl;
-        return QString("help/en/index.html");
-    }
+    #if (defined (_WIN32) || defined (_WIN64))
+        QString path = QString("help/" + language + "/index.html");
+
+        if (stat (path.toStdString().c_str(), &buffer) == 0)
+            return path;
+        else
+        {
+            std::cout << "Localized help for language " << language.toStdString() << " not found, defaulting to en" << std::endl;
+            return QString("help/en/index.html");
+        }
+    #elif (defined (LINUX) || defined (__linux__) || defined(__APPLE__))
+        QString path = QString("/usr/share/pwdmanager-qt/help/" + language + "/index.html");
+
+        if (stat (path.toStdString().c_str(), &buffer) == 0)
+            return path;
+        else
+        {
+            path = QString("/usr/local/share/pwdmanager-qt/help/" + language + "/index.html");
+
+            if (stat (path.toStdString().c_str(), &buffer) == 0)
+                return path;
+            else
+            {
+                std::cout << "Localized help for language " << language.toStdString() << " not found, defaulting to en" << std::endl;
+                return QString("help/en/index.html");
+            }
+        }
+    #endif
 }
 
 int IsDir()
